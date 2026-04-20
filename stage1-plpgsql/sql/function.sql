8e2aa17af8df45d8795c26de0ce3fe2b21d70674
elizarion <asyelizaro@gmail.com>
2026-04-20 21:28:08 +0300

    add fn_fact_sales

--- stage1-plpgsql/sql/function.sql
+++ stage1-plpgsql/sql/function.sql
@@ -1,36 +1,40 @@
-INSERT INTO dds.fact_sales
-(
-	sale_id,
-	invoice_id,
-	customer_id,
-	track_id,
-	artist_id,
-	album_id,
-	invoice_date,
-	unit_price,
-	quantity,
-	total_amount,
-	created_at
-)
-SELECT
-    il.invoice_line_id AS sale_id,
-    i.invoice_id,
-    i.customer_id,
-    il.track_id,
-    al.artist_id,
-    t.album_id,
-    i.invoice_date::date AS invoice_date,
-    il.unit_price,
-    il.quantity,
-    il.unit_price * il.quantity AS total_amount,
-    NOW() AS created_at
-FROM 
-	stage.invoice_line il
-JOIN stage.invoice i 
-	ON il.invoice_id = i.invoice_id
-JOIN stage.track t 
-	ON il.track_id = t.track_id
-JOIN stage.album al
-	ON al.album_id = t.album_id
-ORDER BY 
-	i.invoice_date, i.invoice_id, il.invoice_line_id;
+CREATE OR REPLACE FUNCTION fn_fact_sales()
+RETURNS NULL
+LANGUAGE plpgsql
+AS $$
+BEGIN
+
+    INSERT INTO dds.fact_sales (
+        sale_id,
+        invoice_id,
+        customer_id,
+        track_id,
+        artist_id,
+        album_id,
+        invoice_date,
+        unit_price,
+        quantity,
+        total_amount,
+        created_at
+    )
+    SELECT
+        il.invoice_line_id AS sale_id,
+        i.invoice_id,
+        i.customer_id,
+        il.track_id,
+        al.artist_id,
+        t.album_id,
+        i.invoice_date::DATE AS invoice_date,
+        il.unit_price,
+        il.quantity,
+        il.unit_price * il.quantity AS total_amount,
+        NOW() AS created_at
+    FROM
+        stage.invoice_line il
+    JOIN stage.invoice i ON il.invoice_id = i.invoice_id
+    JOIN stage.track t ON il.track_id = t.track_id
+    JOIN stage.album al ON al.album_id = t.album_id
+    ORDER BY
+        i.invoice_date, i.invoice_id, il.invoice_line_id;
+
+END $$;

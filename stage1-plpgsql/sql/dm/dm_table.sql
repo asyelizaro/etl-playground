3b9b3cd3007886ea3380b0690f4f5533ff2110d1
elizarion <asyelizaro@gmail.com>
2026-05-02 13:47:54 +0300

    add t_dm_sales_by_artist and calculate fuction

--- stage1-plpgsql/sql/dm/dm_table.sql
+++ stage1-plpgsql/sql/dm/dm_table.sql
@@ -1,6 +1,10 @@
 -- содание схемы и таблиц data_mart слоя
 CREATE SCHEMA IF NOT EXISTS data_mart;
 
+DROP TABLE IF EXISTS data_mart.t_dm_sales_customer;
+
+DROP TABLE IF EXISTS data_mart.t_dm_sales_by_artist;
+
 CREATE TABLE IF NOT EXISTS data_mart.t_dm_sales_customer (
     customer_id int4,
     full_name text,
@@ -10,3 +14,12 @@ CREATE TABLE IF NOT EXISTS data_mart.t_dm_sales_customer (
     revenue numeric(12, 2),
     quanity int8
 );
+
+CREATE TABLE IF NOT EXISTS data_mart.t_dm_sales_by_artist (
+    invoice_date date,
+    artist_name varchar(80),
+    genre_name varchar(120),
+    avg_price_track numeric(12, 2),
+    revenue numeric(12, 2),
+    quantity numeric(12, 2)
+)
---
+++ stage1-plpgsql/sql/dm/fn_dm_sales_by_artist.sql
@@ -0,0 +1,36 @@
+CREATE OR REPLACE FUNCTION data_mart.fn_dm_sales_by_artist()
+RETURNS VOID
+LANGUAGE plpgsql
+AS $$
+BEGIN
+
+    INSERT INTO data_mart.t_dm_sales_by_artist (
+        invoice_date,
+        artist_name,
+        genre_name,
+        avg_price_track,
+        revenue,
+        quantity
+    )
+    SELECT
+        fct.invoice_date,
+        fct.artist_id,
+        g.name AS genre_name,
+        AVG(fct.unit_price) AS avg_price_track,
+        SUM(fct.total_amount) AS revenue,
+        SUM(fct.quantity) AS quantity
+    FROM
+        dds.fact_sales AS fct
+    JOIN dds.dim_track AS t
+        ON t.track_id = fct.track_id
+    JOIN dds.dim_genre AS g
+        ON g.genre_id = t.genre_id
+    JOIN dds.dim_artist AS a
+        ON a.artist_id = fct.artist_id
+    GROUP BY
+        1, 2, 3;
+
+
+
+
+END $$;

9c74c675eee7f91dd085baeec7f2b99d23b5a397
elizarion <asyelizaro@gmail.com>
2026-06-04 21:06:07 +0300

    add bash script for rin stage1

---
+++ stage1-plpgsql/start-stage1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+echo "Starting Chinook database..."
+cd stage0-source
+docker-compose up -d
+
+echo "Starting ETL environment..."
+cd ../stage1-plpgsql
+docker-compose up -d
+
+echo "Done. Open Airflow: http://localhost:8080/home"
